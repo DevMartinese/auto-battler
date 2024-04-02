23f52f9ebe3df9707343fcc88b578b6bf4a656be
DevMartinese <devmartinese@gmail.com>
2024-04-02 20:52:42 -0300

    delete App.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,31 @@ import { TMLoadModel } from "./component/tmLoadModel";
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [0, 3, 3] }} shadows>
+      <Canvas 
+        camera={{ position: [0, 11, 11] }} 
+        shadows
+      >
         <OrbitControls />
 
-        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
-        <ambientLight intensity={0.5} />
+        <directionalLight 
+          castShadow 
+          position={[1, 2, 3]} 
+          intensity={1.5} 
+        />
+        <ambientLight 
+          intensity={0.5} 
+        />
 
-        <BmpLoadModel position-y={-1} position-z={-5} />
-        <TMLoadModel position-y={-1} position-z={7} position-x={-1.7} rotation-y={2} />
+        <BmpLoadModel 
+          position-y={-1} 
+          position-z={-5} 
+        />
+        <TMLoadModel 
+          position-y={-1} 
+          position-z={7} 
+          position-x={-1.7} 
+          rotation-y={2} 
+        />
 
         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={2} receiveShadow>
           <planeGeometry args={[10, 10]} />
