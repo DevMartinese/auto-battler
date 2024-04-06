d3ba48e84d4df4683260a272970fa7bd1bd5d35a
DevMartinese <devmartinese@gmail.com>
2024-04-06 13:15:50 -0300

    Debugging model

--- src/component/BMP/Caterpillar.jsx
+++ src/component/BMP/Caterpillar.jsx
@@ -6,18 +6,23 @@ import { MeshNormalMaterial } from 'three'
 export const Caterpillar = (props) => {
   const { nodes, materials } = useGLTF('models/uploads_files_3839708_Modification.glb');
 
-  console.log("NODES====>", nodes);
+  const positionsArray = nodes.caterpillar.geometry.attributes.position.array;
+  const hasNaN = positionsArray.some(value => isNaN(value));
+  console.log(hasNaN)
+  if (hasNaN) {
+    console.error('La geometría tiene valores NaN', positionsArray);
+  }
 
-  // const [ref] = useTrimesh(() => ({
-  //   args: [nodes.caterpillar.geometry.attributes.position.array, nodes.caterpillar.geometry.index.array],
-  //   mass: 1,
-  //   position: [0.445, 0.017, 0.605],
-  //   rotation: [0, 0, -Math.PI / 2]
-  // }),
-  //   useRef())
+  const [ref] = useTrimesh(() => ({
+    args: [nodes.caterpillar.geometry.attributes.position.array, nodes.caterpillar.geometry.index.array],
+    mass: 1,
+    position: [0.445, 0.017, 0.605],
+    rotation: [0, 0, -Math.PI / 2]
+  }),
+    useRef())
 
   return (
-    <mesh 
+    <mesh ref={ref}
       castShadow
       receiveShadow
       geometry={nodes.caterpillar.geometry}
