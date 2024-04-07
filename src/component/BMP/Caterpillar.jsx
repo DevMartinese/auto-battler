835631a5fc5b63323e45d25b655f11e5ef2a11df
DevMartinese <devmartinese@gmail.com>
2024-04-07 19:35:28 -0300

    fix

--- src/component/BMP/Caterpillar.jsx
+++ src/component/BMP/Caterpillar.jsx
@@ -2,24 +2,9 @@ import { useGLTF } from '@react-three/drei';
 
 export const Caterpillar = () => {
   const { nodes, materials } = useGLTF('models/uploads_files_3839708_Modification.glb');
-
-  const positionsArray = nodes.caterpillar.geometry.attributes.position.array;
-  const hasNaN = positionsArray.some(value => isNaN(value));
-  console.log(hasNaN)
-  if (hasNaN) {
-    console.error('La geometría tiene valores NaN', positionsArray);
-  }
-
-  const [ref] = useTrimesh(() => ({
-    args: [nodes.caterpillar.geometry.attributes.position.array, nodes.caterpillar.geometry.index.array],
-    mass: 1,
-    position: [0.445, 0.017, 0.605],
-    rotation: [0, 0, -Math.PI / 2]
-  }),
-    useRef())
-
+  
   return (
-    <mesh ref={ref}
+    <mesh
       castShadow
       receiveShadow
       geometry={nodes.caterpillar.geometry}
