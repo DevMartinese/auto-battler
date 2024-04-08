a40f6f1bf72ca226db6258799db733f3f017cf02
DevMartinese <devmartinese@gmail.com>
2024-04-08 19:10:27 -0300

    add: RigidBody on Wheels

--- src/component/BMP/Wheel1.jsx
+++ src/component/BMP/Wheel1.jsx
@@ -1,23 +1,19 @@
 import { useGLTF } from '@react-three/drei';
-import { CuboidCollider, RigidBody } from '@react-three/rapier';
+import { RigidBody } from '@react-three/rapier';
 
 export const Wheel1 = () => {
   const { nodes, materials } = useGLTF('models/uploads_files_3839708_Modification.glb');
-  const wheelPosition = [0.449, 0.124, 0.437]; // Asumiendo que esta es la posición correcta de la rueda
-  const wheelSize = [0.5, 0.5, 0.5]; // Tendrías que ajustar esto a las dimensiones reales de la rueda
+  const wheelPosition = [0.449, 0.124, 0.437];
 
   return (
-    <RigidBody colliders="cuboid" position={[1, 5, 2]}>
-      <CuboidCollider args={wheelSize}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.wheel_3.geometry}
-          material={materials['cgbookcase.painted-metal-03_png_1k_']}
-          position={wheelPosition}
-          rotation={[0, 0, -Math.PI / 2]}
-        />
-      </CuboidCollider>
+    <RigidBody colliders="trimesh" position={wheelPosition}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.wheel_3.geometry}
+        material={materials['cgbookcase.painted-metal-03_png_1k_']}
+        rotation={[0, 0, -Math.PI / 2]}
+      />
     </RigidBody>
   );
 };
