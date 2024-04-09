0b064387576319d5eb1154a3caef78c9c47697db
DevMartinese <devmartinese@gmail.com>
2024-04-08 22:02:13 -0300

    add: physics in Wheels

--- src/component/BMP/Wheel1.jsx
+++ src/component/BMP/Wheel1.jsx
@@ -2,25 +2,17 @@ import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
-export const Wheel1 = () => {
+export const Wheel1 = ({ wheel1Ref }) => {
   const { nodes, materials } = useGLTF('models/uploads_files_3839708_Modification.glb');
-  const wheelRef = useRef();
-
-  // Rotar la rueda en cada cuadro
-  useFrame(() => {
-    if (wheelRef.current) {
-      wheelRef.current.position.z += 0.01; // Ajusta este valor para controlar la velocidad de rotación
-    }
-  });
-
+  
   return (
-      <mesh
-        ref={wheelRef}
-        castShadow
-        receiveShadow
-        geometry={nodes.wheel_3.geometry}
-        material={materials['cgbookcase.painted-metal-03_png_1k_']}
-        rotation={[0, 0, -Math.PI / 2]}
-      />
+    <mesh
+      ref={wheel1Ref}
+      castShadow
+      receiveShadow
+      geometry={nodes.wheel_3.geometry}
+      material={materials['cgbookcase.painted-metal-03_png_1k_']}
+      rotation={[0, 0, -Math.PI / 2]}
+    />
   );
 };
--- src/component/BMP/Wheels.jsx
+++ src/component/BMP/Wheels.jsx
@@ -1,18 +1,29 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
-import { Caterpillar } from "./Caterpillar"
 import { Wheel1 } from "./Wheel1"
 import { Wheel2 } from "./Wheel2"
+import { Caterpillar } from "./Caterpillar"
 
 export const Wheels = () => {
-  const wheelPosition = [0.449, 0.124, 0.437];
+  const wheel1Position = [0.449, 0.124, 0.437];
+
+  const wheel1Ref = useRef();
+
+  // Rotar la rueda en cada cuadro
+  useFrame(() => {
+    if (wheel1Ref.current) {
+      wheel1Ref.current.position.z += 0.01; // Ajusta este valor para controlar la velocidad de rotación
+    }
+  });
 
   return (
     <group>
       <RigidBody
         colliders="trimesh"
-        position={wheelPosition}
+        position={wheel1Position}
       >
-        <Wheel1 />
+        <Wheel1 wheel1Ref={wheel1Ref} />
       </RigidBody>
       {/* <Wheel2 />
       <Caterpillar /> */}
